88b43e981b4d5cd324158ec783c4d3f86e7f737d
wxlkda <674445@pdsb.net>
2023-05-04 18:34:08 -0400

    Updated buttons to transition between the two modes. Might add search mode, idk

--- src/main.js
+++ src/main.js
@@ -1,1 +1,40 @@
-
+document.addEventListener('DOMContentLoaded', (event) => {
+    var sortButton = document.getElementById("sortButton");
+    var traverseButton = document.getElementById("traverseButton");
+  
+    var sortDiv = document.getElementById("sort");
+    var traverseDiv = document.getElementById("traverse");
+  
+    var selected = 0;
+  
+    sortButton.addEventListener('click', toggleSortClass);
+    traverseButton.addEventListener('click', toggleTraverseClass);
+  
+    function toggleSortClass() {
+      selected = 0;
+      updateButtonClasses();
+    }
+  
+    function toggleTraverseClass() {
+      selected = 1;
+      updateButtonClasses();
+    }
+  
+    function updateButtonClasses() {
+      if (selected == 0) {
+        sortButton.className = 'selection-button sort-selected';
+        traverseButton.className = "selection-button traversal";
+        sortDiv.style.display = "flex";
+        traverseDiv.style.display = "none";
+      } else if (selected == 1) {
+        traverseButton.className = 'selection-button traversal-selected';
+        sortButton.className = "selection-button sort";
+        sortDiv.style.display = "none";
+        traverseDiv.style.display = "flex";
+      }
+    }
+  
+    // Set initial button classes and div visibility
+    updateButtonClasses();
+  });
+  
